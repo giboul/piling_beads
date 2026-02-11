0ebc978b86ba82f9893b395997e0a53424872e3f
giboul <axel.giboulot@epfl.ch>
2026-02-11 18:33:15 +0100

    .

--- ipile.py
+++ ipile.py
@@ -1,38 +1,125 @@
+import numpy as np
 import pyvista as pv
 from resting_bead import resting_bead
 import traceback
 
+class AttrDict(dict):
+    def __getattr__(self, key):
+        return self[key]
+    def __setattr__(self, key, value):
+        self[key] = value
+    def __repr__(self):
+        return "<--AttrDict\n"+"\n".join([f"{k}: {v}" for k, v in self.items()])+"\n-->"
 
-state = dict(radius=1, num_spheres=0, spheres=[], selected_spheres=[])
-
-def add_sphere(radius, center, mesh_kwargs=dict(), sphere_kwargs=dict()):
-    state["spheres"].append((radius, *center))
-    mesh = pv.Sphere(radius=radius, center=center, **sphere_kwargs)
-    mesh._id = state["num_spheres"]
-    state["num_spheres"] += 1
-    pl.add_mesh(mesh, **mesh_kwargs)
+state = AttrDict(
+    r = 1.,
+    ids = np.zeros((0,), dtype=np.int64),
+    spheres = np.zeros((0, 4), dtype=np.float64),
+    selected_mesh = None,
+    selected_spheres = set(),
+    mode = "select"
+)
+print(state)
 
 def update_radius(value) -> None:
-    state["radius"] = round(value, 0)
+    state.radius = value
 
-def mesh_pick(mesh):
-    state["selected_spheres"] += state["spheres"][mesh._id]
-    if len(state["selected_spheres"])//4 == 3:
+def add_sphere(radius, center, mesh_kwargs=dict(), sphere_kwargs=dict()):
+    if not check_conflicts(radius, center):
+        raise ValueError("There is a conflict with another sphere !")
+    state.spheres = np.vstack((state.spheres, (radius, *center)))
+    mesh = pv.Sphere(radius=radius, center=center, **sphere_kwargs)
+    ix = state.ids.max()+1 if state.ids.size else 0
+    state.ids = np.hstack((state.ids, ix), dtype=np.int64)
+    mesh._id = state.ids.size - 1
+    mesh._actor = pl.add_mesh(mesh, pickable=True, **mesh_kwargs)
+
+def check_conflicts(r, xyz, tol=1e-5):
+    radii = state.spheres[:, 0]
+    centers = state.spheres[:, 1:]
+    dist2 = ((centers - xyz)**2).sum(axis=1)
+    conflict = (dist2 < (radii+r-tol)**2)
+    return not conflict.any()
+
+def drop_sphere():
         try:
-            center = resting_bead(state["radius"], *state["selected_spheres"])
-            add_sphere(state["radius"], center, mesh_kwargs=dict(color="red"))
+            spheres_data = [p for s in state.selected_spheres for p in state.spheres[s]]
+            center = resting_bead(state.radius, *spheres_data)
+            add_sphere(state.radius, center, mesh_kwargs=dict(color="red"))
         except ValueError as e:
             traceback.print_tb(e.__traceback__)
-            print("ERROR: Solution cannot be computed.", )
+            print("ERROR: Solution cannot be computed.")
         finally:
-            state["selected_spheres"].clear()
+            state.selected_spheres.clear()
 
+def delete_sphere():
+    mesh = state.selected_mesh
+    ix = state.ids[mesh._id]
+    print(f"{state.ids = }, {mesh._id = }, {ix = }")
+    print("#"*10)
+    print(state)
+    state.spheres = np.vstack([state.spheres[:ix], state.spheres[ix+1:]])
+    print(state.spheres)
+    # state.ids = state.ids[state.ids != mesh._id]
+    a = np.arange(state.ids.size)
+    state.ids[a == mesh._id] = -1
+    state.ids[a > mesh._id] -= 1
+    print(f"{state.ids = }, {mesh._id = }, {ix = }")
+    print("/"*10 + "\n")
+    pl.remove_actor(mesh._actor)
+    pl.render()
+    state.mode = "select"
+    state.selected_mesh = None
+    state.selected_spheres.clear()
+
+def clear_selection():
+    print("Clearing")
+    state.selected_spheres.clear()
+
+def mesh_pick(mesh):
+    if not hasattr(mesh, "_id"):
+        return
+    if state.mode == "delete":
+        return delete_sphere(mesh)
+    state.selected_mesh = mesh
+    state.selected_spheres.add(state.ids[mesh._id])
+    if len(state.selected_spheres) == 3:
+        drop_sphere()
+
+def add_bed_sphere(point):
+    point[2] = state.radius
+    add_sphere(state.radius, point)
+
+def toggle_bed_mode():
+    pl.disable_picking()
+    pl.enable_surface_point_picking(add_bed_sphere, left_clicking=True)
+    state.mode = "bed"
+
+def toggle_select_mode():
+    pl.disable_picking()
+    pl.enable_mesh_picking(mesh_pick, left_clicking=True)
+    state.mode = "select"
+
+x = np.linspace(-10, 10, num=1_000)
+y = np.linspace(-10, 10, num=1_000)
+x, y = np.meshgrid(x, y)
+z = np.zeros_like(x)
 pl = pv.Plotter()
-add_sphere(radius=1, center=(0, 0, 0))
-add_sphere(radius=1, center=(2, 0, 0))
-add_sphere(radius=1, center=(0, 2, 0))
-add_sphere(radius=1, center=(0, 0, 2))
+pl.add_mesh(pv.StructuredGrid(x, y, z), opacity=0.5)
+add_sphere(radius=1, center=(0, 0, 1))
+add_sphere(radius=1, center=(2, 0, 1))
+add_sphere(radius=1, center=(0, 2, 1))
+add_sphere(radius=1, center=(-2, 0, 1))
+add_sphere(radius=1, center=(0, -2, 1))
+add_sphere(radius=1, center=(0, -4, 1))
+add_sphere(radius=1, center=(-4, 0, 1))
+add_sphere(radius=1, center=(-2, -2, 1))
+add_sphere(radius=1, center=(2, 2, 1))
 pl.add_slider_widget(update_radius, [1, 5], title="Radius")
-pl.enable_mesh_picking(mesh_pick, left_clicking=True)
+pl.add_key_event("d", delete_sphere)
+pl.add_key_event("b", toggle_bed_mode)
+pl.add_key_event("s", toggle_select_mode)
+pl.add_key_event("Escape", clear_selection)
+toggle_select_mode()
 pl.show_axes()
 pl.show()
--- resting_bead.py
+++ resting_bead.py
@@ -2,60 +2,80 @@
 Module for finding the position of a single sphere of known radius 
 in tangent contact with three other spheres.
 
-Use the `resting_bead` function for functionnality or 
-`naive_resting_bead` for direct but unsteady calculation.
+Use the `resting_bead` function or `naive_resting_bead` for naive calculation.
 """
 import numpy as np
 
 
-def naive_resting_bead(r, r_1, x_1, y_1, z_1, r_2, x_2, y_2, z_2, r_3, x_3, y_3, z_3, s=+1):
+def naive_resting_bead(r, r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3, s=+1):
     """
     From the radius of the new sphere and the coordinates and radii of three other spheres,
     find the position of the tangent fourth sphere. This solution is not valid for cases where 
-    x_1 == x_2 or the centers are on a vertical plane (xz or yz).
-    
+    x1 == x2 or the centers are on a vertical plane (xz or yz).
+
     :param r: Radius of the new sphere
-    :param r_1: Radius of the first sphere
-    :param x_1: `x` coordinate of the first sphere
-    :param y_1: `y` coordinate of the first sphere
-    :param z_1: `z` coordinate of the first sphere
-    :param r_2: Radius of the second sphere
-    :param x_2: `x` coordinate of the second sphere
-    :param y_2: `y` coordinate of the second sphere
-    :param z_2: `z` coordinate of the second sphere
-    :param r_3: Radius of the third sphere
-    :param x_3: `x` coordinate of the third sphere
-    :param y_3: `y` coordinate of the third sphere
-    :param z_3: `z` coordinate of the third sphere
+    :param r1: Radius of the first sphere
+    :param x1: `x` coordinate of the first sphere
+    :param y1: `y` coordinate of the first sphere
+    :param z1: `z` coordinate of the first sphere
+    :param r2: Radius of the second sphere
+    :param x2: `x` coordinate of the second sphere
+    :param y2: `y` coordinate of the second sphere
+    :param z2: `z` coordinate of the second sphere
+    :param r3: Radius of the third sphere
+    :param x3: `x` coordinate of the third sphere
+    :param y3: `y` coordinate of the third sphere
+    :param z3: `z` coordinate of the third sphere
     :param s: Which solution to keep: s=+1 top solution, s=-1 -> bottom solution
     """
-    z =(s*(((-2*z_1*(-x_1*y_2 + x_1*y_3 + x_2*y_1 - x_2*y_3 - x_3*y_1 + x_3*y_2)**2 + (x_1*z_2 - x_1*z_3 - x_2*z_1 + x_2*z_3 + x_3*z_1 - x_3*z_2)*(-2*r*r_1*x_2 + 2*r*r_1*x_3 + 2*r*r_2*x_1 - 2*r*r_2*x_3 - 2*r*r_3*x_1 + 2*r*r_3*x_2 - r_1**2*x_2 + r_1**2*x_3 + r_2**2*x_1 - r_2**2*x_3 - r_3**2*x_1 + r_3**2*x_2 + x_1**2*x_2 - x_1**2*x_3 - x_1*x_2**2 + x_1*x_3**2 + 2*x_1*y_1*y_2 - 2*x_1*y_1*y_3 - x_1*y_2**2 + x_1*y_3**2 - x_1*z_2**2 + x_1*z_3**2 + x_2**2*x_3 - x_2*x_3**2 - x_2*y_1**2 + 2*x_2*y_1*y_3 - x_2*y_3**2 + x_2*z_1**2 - x_2*z_3**2 + x_3*y_1**2 - 2*x_3*y_1*y_2 + x_3*y_2**2 - x_3*z_1**2 + x_3*z_2**2) + (-y_1*z_2 + y_1*z_3 + y_2*z_1 - y_2*z_3 - y_3*z_1 + y_3*z_2)*(2*r*r_1*y_2 - 2*r*r_1*y_3 - 2*r*r_2*y_1 + 2*r*r_2*y_3 + 2*r*r_3*y_1 - 2*r*r_3*y_2 + r_1**2*y_2 - r_1**2*y_3 - r_2**2*y_1 + r_2**2*y_3 + r_3**2*y_1 - r_3**2*y_2 + x_1**2*y_2 - x_1**2*y_3 - 2*x_1*x_2*y_1 + 2*x_1*x_2*y_3 + 2*x_1*x_3*y_1 - 2*x_1*x_3*y_2 + x_2**2*y_1 - x_2**2*y_3 - x_3**2*y_1 + x_3**2*y_2 - y_1**2*y_2 + y_1**2*y_3 + y_1*y_2**2 - y_1*y_3**2 + y_1*z_2**2 - y_1*z_3**2 - y_2**2*y_3 + y_2*y_3**2 - y_2*z_1**2 + y_2*z_3**2 + y_3*z_1**2 - y_3*z_2**2))**2 - ((-x_1*y_2 + x_1*y_3 + x_2*y_1 - x_2*y_3 - x_3*y_1 + x_3*y_2)**2 + (x_1*z_2 - x_1*z_3 - x_2*z_1 + x_2*z_3 + x_3*z_1 - x_3*z_2)**2 + (-y_1*z_2 + y_1*z_3 + y_2*z_1 - y_2*z_3 - y_3*z_1 + y_3*z_2)**2)*(4*z_1**2*(-x_1*y_2 + x_1*y_3 + x_2*y_1 - x_2*y_3 - x_3*y_1 + x_3*y_2)**2 - 4*(r + r_1)**2*(-x_1*y_2 + x_1*y_3 + x_2*y_1 - x_2*y_3 - x_3*y_1 + x_3*y_2)**2 + (-2*r*r_1*x_2 + 2*r*r_1*x_3 + 2*r*r_2*x_1 - 2*r*r_2*x_3 - 2*r*r_3*x_1 + 2*r*r_3*x_2 - r_1**2*x_2 + r_1**2*x_3 + r_2**2*x_1 - r_2**2*x_3 - r_3**2*x_1 + r_3**2*x_2 + x_1**2*x_2 - x_1**2*x_3 - x_1*x_2**2 + x_1*x_3**2 + 2*x_1*y_1*y_2 - 2*x_1*y_1*y_3 - x_1*y_2**2 + x_1*y_3**2 - x_1*z_2**2 + x_1*z_3**2 + x_2**2*x_3 - x_2*x_3**2 - x_2*y_1**2 + 2*x_2*y_1*y_3 - x_2*y_3**2 + x_2*z_1**2 - x_2*z_3**2 + x_3*y_1**2 - 2*x_3*y_1*y_2 + x_3*y_2**2 - x_3*z_1**2 + x_3*z_2**2)**2 + (2*r*r_1*y_2 - 2*r*r_1*y_3 - 2*r*r_2*y_1 + 2*r*r_2*y_3 + 2*r*r_3*y_1 - 2*r*r_3*y_2 + r_1**2*y_2 - r_1**2*y_3 - r_2**2*y_1 + r_2**2*y_3 + r_3**2*y_1 - r_3**2*y_2 + x_1**2*y_2 - x_1**2*y_3 - 2*x_1*x_2*y_1 + 2*x_1*x_2*y_3 + 2*x_1*x_3*y_1 - 2*x_1*x_3*y_2 + x_2**2*y_1 - x_2**2*y_3 - x_3**2*y_1 + x_3**2*y_2 - y_1**2*y_2 + y_1**2*y_3 + y_1*y_2**2 - y_1*y_3**2 + y_1*z_2**2 - y_1*z_3**2 - y_2**2*y_3 + y_2*y_3**2 - y_2*z_1**2 + y_2*z_3**2 + y_3*z_1**2 - y_3*z_2**2)**2))/(-x_1*y_2 + x_1*y_3 + x_2*y_1 - x_2*y_3 - x_3*y_1 + x_3*y_2)**4)**0.5*(-x_1*y_2 + x_1*y_3 + x_2*y_1 - x_2*y_3 - x_3*y_1 + x_3*y_2)**2 + 2*z_1*(-x_1*y_2 + x_1*y_3 + x_2*y_1 - x_2*y_3 - x_3*y_1 + x_3*y_2)**2 - (x_1*z_2 - x_1*z_3 - x_2*z_1 + x_2*z_3 + x_3*z_1 - x_3*z_2)*(-2*r*r_1*x_2 + 2*r*r_1*x_3 + 2*r*r_2*x_1 - 2*r*r_2*x_3 - 2*r*r_3*x_1 + 2*r*r_3*x_2 - r_1**2*x_2 + r_1**2*x_3 + r_2**2*x_1 - r_2**2*x_3 - r_3**2*x_1 + r_3**2*x_2 + x_1**2*x_2 - x_1**2*x_3 - x_1*x_2**2 + x_1*x_3**2 + 2*x_1*y_1*y_2 - 2*x_1*y_1*y_3 - x_1*y_2**2 + x_1*y_3**2 - x_1*z_2**2 + x_1*z_3**2 + x_2**2*x_3 - x_2*x_3**2 - x_2*y_1**2 + 2*x_2*y_1*y_3 - x_2*y_3**2 + x_2*z_1**2 - x_2*z_3**2 + x_3*y_1**2 - 2*x_3*y_1*y_2 + x_3*y_2**2 - x_3*z_1**2 + x_3*z_2**2) - (-y_1*z_2 + y_1*z_3 + y_2*z_1 - y_2*z_3 - y_3*z_1 + y_3*z_2)*(2*r*r_1*y_2 - 2*r*r_1*y_3 - 2*r*r_2*y_1 + 2*r*r_2*y_3 + 2*r*r_3*y_1 - 2*r*r_3*y_2 + r_1**2*y_2 - r_1**2*y_3 - r_2**2*y_1 + r_2**2*y_3 + r_3**2*y_1 - r_3**2*y_2 + x_1**2*y_2 - x_1**2*y_3 - 2*x_1*x_2*y_1 + 2*x_1*x_2*y_3 + 2*x_1*x_3*y_1 - 2*x_1*x_3*y_2 + x_2**2*y_1 - x_2**2*y_3 - x_3**2*y_1 + x_3**2*y_2 - y_1**2*y_2 + y_1**2*y_3 + y_1*y_2**2 - y_1*y_3**2 + y_1*z_2**2 - y_1*z_3**2 - y_2**2*y_3 + y_2*y_3**2 - y_2*z_1**2 + y_2*z_3**2 + y_3*z_1**2 - y_3*z_2**2))/(2*((-x_1*y_2 + x_1*y_3 + x_2*y_1 - x_2*y_3 - x_3*y_1 + x_3*y_2)**2 + (x_1*z_2 - x_1*z_3 - x_2*z_1 + x_2*z_3 + x_3*z_1 - x_3*z_2)**2 + (-y_1*z_2 + y_1*z_3 + y_2*z_1 - y_2*z_3 - y_3*z_1 + y_3*z_2)**2)) 
-    y = ((-x_1 + x_2)*(-x_1**2 + x_3**2 - y_1**2 + y_3**2 + 2*z*(z_1 - z_3) - z_1**2 + z_3**2 + (r + r_1)**2 - (r + r_3)**2) - (-x_1 + x_3)*(-x_1**2 + x_2**2 - y_1**2 + y_2**2 + 2*z*(z_1 - z_2) - z_1**2 + z_2**2 + (r + r_1)**2 - (r + r_2)**2))/(2*((-x_1 + x_2)*(-y_1 + y_3) - (-x_1 + x_3)*(-y_1 + y_2)))
-    x = (-(-y_1 + y_2)*((-x_1 + x_2)*(-x_1**2 + x_3**2 - y_1**2 + y_3**2 + 2*z*(z_1 - z_3) - z_1**2 + z_3**2 + (r + r_1)**2 - (r + r_3)**2) - (-x_1 + x_3)*(-x_1**2 + x_2**2 - y_1**2 + y_2**2 + 2*z*(z_1 - z_2) - z_1**2 + z_2**2 + (r + r_1)**2 - (r + r_2)**2)) + ((-x_1 + x_2)*(-y_1 + y_3) - (-x_1 + x_3)*(-y_1 + y_2))*(-x_1**2 + x_2**2 - y_1**2 + y_2**2 + 2*z*(z_1 - z_2) - z_1**2 + z_2**2 + (r + r_1)**2 - (r + r_2)**2))/(2*(-x_1 + x_2)*((-x_1 + x_2)*(-y_1 + y_3) - (-x_1 + x_3)*(-y_1 + y_2)))
+    z = (s*(((-2*z1*(-x1*y2 + x1*y3 + x2*y1 - x2*y3 - x3*y1 + x3*y2)**2 + (x1*z2 - x1*z3 - x2*z1 + x2*z3 + x3*z1 - x3*z2)*(-2*r*r1*x2 + 2*r*r1*x3 + 2*r*r2*x1 - 2*r*r2*x3 - 2*r*r3*x1 + 2*r*r3*x2 - r1**2*x2 + r1**2*x3 + r2**2*x1 - r2**2*x3 - r3**2*x1 + r3**2*x2 + x1**2*x2 - x1**2*x3 - x1*x2**2 + x1*x3**2 + 2*x1*y1*y2 - 2*x1*y1*y3 - x1*y2**2 + x1*y3**2 - x1*z2**2 + x1*z3**2 + x2**2*x3 - x2*x3**2 - x2*y1**2 + 2*x2*y1*y3 - x2*y3**2 + x2*z1**2 - x2*z3**2 + x3*y1**2 - 2*x3*y1*y2 + x3*y2**2 - x3*z1**2 + x3*z2**2) + (-y1*z2 + y1*z3 + y2*z1 - y2*z3 - y3*z1 + y3*z2)*(2*r*r1*y2 - 2*r*r1*y3 - 2*r*r2*y1 + 2*r*r2*y3 + 2*r*r3*y1 - 2*r*r3*y2 + r1**2*y2 - r1**2*y3 - r2**2*y1 + r2**2*y3 + r3**2*y1 - r3**2*y2 + x1**2*y2 - x1**2*y3 - 2*x1*x2*y1 + 2*x1*x2*y3 + 2*x1*x3*y1 - 2*x1*x3*y2 + x2**2*y1 - x2**2*y3 - x3**2*y1 + x3**2*y2 - y1**2*y2 + y1**2*y3 + y1*y2**2 - y1*y3**2 + y1*z2**2 - y1*z3**2 - y2**2*y3 + y2*y3**2 - y2*z1**2 + y2*z3**2 + y3*z1**2 - y3*z2**2))**2 - ((-x1*y2 + x1*y3 + x2*y1 - x2*y3 - x3*y1 + x3*y2)**2 + (x1*z2 - x1*z3 - x2*z1 + x2*z3 + x3*z1 - x3*z2)**2 + (-y1*z2 + y1*z3 + y2*z1 - y2*z3 - y3*z1 + y3*z2)**2)*(4*z1**2*(-x1*y2 + x1*y3 + x2*y1 - x2*y3 - x3*y1 + x3*y2)**2 - 4*(r + r1)**2*(-x1*y2 + x1*y3 + x2*y1 - x2*y3 - x3*y1 + x3*y2)**2 + (-2*r*r1*x2 + 2*r*r1*x3 + 2*r*r2*x1 - 2*r*r2*x3 - 2*r*r3*x1 + 2*r*r3*x2 - r1**2*x2 + r1**2*x3 + r2**2*x1 - r2**2*x3 - r3**2*x1 + r3**2*x2 + x1**2*x2 - x1**2*x3 - x1*x2**2 + x1*x3**2 + 2*x1*y1*y2 - 2*x1*y1*y3 - x1*y2**2 + x1*y3**2 - x1*z2**2 + x1*z3**2 + x2**2*x3 - x2*x3**2 - x2*y1**2 + 2*x2*y1*y3 - x2*y3**2 + x2*z1**2 - x2*z3**2 + x3*y1**2 - 2*x3*y1*y2 + x3*y2**2 - x3*z1**2 + x3*z2**2)**2 + (2*r*r1*y2 - 2*r*r1*y3 - 2*r*r2*y1 + 2*r*r2*y3 + 2*r*r3*y1 - 2*r*r3*y2 + r1**2*y2 - r1**2*y3 - r2**2*y1 + r2**2*y3 + r3**2*y1 - r3**2*y2 + x1**2*y2 - x1**2*y3 - 2*x1*x2*y1 + 2*x1*x2*y3 + 2*x1*x3*y1 - 2*x1*x3*y2 + x2**2*y1 - x2**2*y3 - x3**2*y1 + x3**2*y2 - y1**2*y2 + y1**2*y3 + y1*y2**2 - y1*y3**2 + y1*z2**2 - y1*z3**2 - y2**2*y3 + y2*y3**2 - y2*z1**2 + y2*z3**2 + y3*z1**2 - y3*z2**2)**2))/(-x1*y2 + x1*y3 + x2*y1 - x2*y3 - x3*y1 + x3*y2)**4)**0.5*(-x1*y2 + x1*y3 + x2*y1 - x2*y3 - x3*y1 + x3*y2)**2 + 2*z1*(-x1*y2 + x1*y3 + x2*y1 - x2*y3 - x3*y1 + x3*y2)**2 - (x1*z2 - x1*z3 - x2*z1 + x2*z3 + x3*z1 - x3*z2)*(-2*r*r1*x2 + 2*r*r1*x3 + 2*r*r2*x1 - 2*r*r2*x3 - 2*r*r3*x1 + 2*r*r3*x2 - r1**2*x2 + r1**2*x3 + r2**2*x1 - r2**2*x3 - r3**2*x1 + r3**2*x2 + x1**2*x2 - x1**2*x3 - x1*x2**2 + x1*x3**2 + 2*x1*y1*y2 - 2*x1*y1*y3 - x1*y2**2 + x1*y3**2 - x1*z2**2 + x1*z3**2 + x2**2*x3 - x2*x3**2 - x2*y1**2 + 2*x2*y1*y3 - x2*y3**2 + x2*z1**2 - x2*z3**2 + x3*y1**2 - 2*x3*y1*y2 + x3*y2**2 - x3*z1**2 + x3*z2**2) - (-y1*z2 + y1*z3 + y2*z1 - y2*z3 - y3*z1 + y3*z2)*(2*r*r1*y2 - 2*r*r1*y3 - 2*r*r2*y1 + 2*r*r2*y3 + 2*r*r3*y1 - 2*r*r3*y2 + r1**2*y2 - r1**2*y3 - r2**2*y1 + r2**2*y3 + r3**2*y1 - r3**2*y2 + x1**2*y2 - x1**2*y3 - 2*x1*x2*y1 + 2*x1*x2*y3 + 2*x1*x3*y1 - 2*x1*x3*y2 + x2**2*y1 - x2**2*y3 - x3**2*y1 + x3**2*y2 - y1**2*y2 + y1**2*y3 + y1*y2**2 - y1*y3**2 + y1*z2**2 - y1*z3**2 - y2**2*y3 + y2*y3**2 - y2*z1**2 + y2*z3**2 + y3*z1**2 - y3*z2**2))/(2*((-x1*y2 + x1*y3 + x2*y1 - x2*y3 - x3*y1 + x3*y2)**2 + (x1*z2 - x1*z3 - x2*z1 + x2*z3 + x3*z1 - x3*z2)**2 + (-y1*z2 + y1*z3 + y2*z1 - y2*z3 - y3*z1 + y3*z2)**2))
+    # y = ((-x1 + x2)*(-x1**2 + x3**2 - y1**2 + y3**2 + 2*z*(z1 - z3) - z1**2 + z3**2 + (r + r1)**2 - (r + r3)**2) - (-x1 + x3)*(-x1**2 + x2**2 - y1**2 + y2**2 + 2*z*(z1 - z2) - z1**2 + z2**2 + (r + r1)**2 - (r + r2)**2))/(2*((-x1 + x2)*(-y1 + y3) - (-x1 + x3)*(-y1 + y2)))
+    # x = (-(-y1 + y2)*((-x1 + x2)*(-x1**2 + x3**2 - y1**2 + y3**2 + 2*z*(z1 - z3) - z1**2 + z3**2 + (r + r1)**2 - (r + r3)**2) - (-x1 + x3)*(-x1**2 + x2**2 - y1**2 + y2**2 + 2*z*(z1 - z2) - z1**2 + z2**2 + (r + r1)**2 - (r + r2)**2)) + ((-x1 + x2)*(-y1 + y3) - (-x1 + x3)*(-y1 + y2))*(-x1**2 + x2**2 - y1**2 + y2**2 + 2*z*(z1 - z2) - z1**2 + z2**2 + (r + r1)**2 - (r + r2)**2))/(2*(-x1 + x2)*((-x1 + x2)*(-y1 + y3) - (-x1 + x3)*(-y1 + y2)))
+    y = ((-2*x1 + 2*x2)*(-x1**2 + x3**2 - y1**2 + y3**2 + z*(2*z1 - 2*z3) - z1**2 + z3**2 + (r + r1)**2 - (r + r3)**2) - (-2*x1 + 2*x3)*(-x1**2 + x2**2 - y1**2 + y2**2 + z*(2*z1 - 2*z2) - z1**2 + z2**2 + (r + r1)**2 - (r + r2)**2))/((-2*x1 + 2*x2)*(-2*y1 + 2*y3) - (-2*x1 + 2*x3)*(-2*y1 + 2*y2))
+    x = -y*(-2*y1 + 2*y2)/(-2*x1 + 2*x2) + (-x1**2 + x2**2 - y1**2 + y2**2 + z*(2*z1 - 2*z2) - z1**2 + z2**2 + (r + r1)**2 - (r + r2)**2)/(-2*x1 + 2*x2)
     return x, y, z
 
-def are_aligned(p1, p2, v):
-    """Check if vector p2-p1 and v do not point to opposite ways."""
-    return np.dot((p2-p1), v) > 0
 
-
-def swap_beads(r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3):
-    """Exchange the first and the thir spheres."""
-    return r3, x3, y3, z3, r2, x2, y2, z2, r1, x1, y1, z1
-
-def swap_axes(r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3):
-    """Work on a plane where the linear solution exists."""
-    if not np.isclose((x2-x1)*(y3-y1) - (x3-x1)*(y2-y1), 0): return "xy", r2, x2, y2, z2, r1, x1, y1, z1, r3, x3, y3, z3
-    if not np.isclose((z2-z1)*(y3-y1) - (z3-z1)*(y2-y1), 0): return "zy", r2, z2, y2, x2, r1, z1, y1, x1, r3, z3, y3, x3
-    if not np.isclose((x2-x1)*(z3-z1) - (x3-x1)*(z2-z1), 0): return "xz", r2, x2, z2, y2, r1, x1, z1, y1, r3, x3, z3, y3
+def swap_axes(r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3, g):
+    """Work in a plane where the linear solution exists."""
+    _g = np.array(g, copy=True)
+    if not np.isclose((x2-x1)*(y3-y1) - (x3-x1)*(y2-y1), 0):
+        return "xy", r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3, _g
+    if not np.isclose((z2-z1)*(y3-y1) - (z3-z1)*(y2-y1), 0):
+        _g[0], _g[2] = _g[2], _g[0]
+        return "zy", r1, z1, y1, x1, r2, z2, y2, x2, r3, z3, y3, x3, _g
+    if not np.isclose((x2-x1)*(z3-z1) - (x3-x1)*(z2-z1), 0):
+        _g[1], _g[2] = _g[2], _g[1]
+        return "xz", r1, x1, z1, y1, r2, x2, z2, y3, r3, x3, z3, y3, _g
     raise ValueError(r"¯\_(ツ)_/¯")
+
 
 def restore_swapped_axes(plane, x, y, z):
     """Restore the original axes from the `swap_axes` transform."""
-    if plane == "xy": return x, y, z
-    if plane == "xz": return x, z, y
-    if plane == "zy": return z, y, x
+    if plane == "xy":
+        return x, y, z
+    if plane == "xz":
+        return x, z, y
+    if plane == "zy":
+        return z, y, x
     raise ValueError(r"¯\_(ツ)_/¯")
+
+
+def is_positive_linear_combination(v1, v2, v3, w):
+    """
+    Check that `w` can be written as a1*v1 + a2*v2 + a3*v3 with ai >= 0.
+
+    :param v1: vector
+    :param v2: vector
+    :param v3: vector
+    :param w: vector
+    :return: True or False
+    :rtype: bool
+    """
+    M = np.array([v1, v2, v3]).T
+    x = np.linalg.solve(M, w)
+    return ((x>=0) | np.isclose(x, 0)).all()
+
 
 def resting_bead(r, r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3, gravity=np.array((0, 0, -1))):
     """
@@ -63,51 +83,49 @@ def resting_bead(r, r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3, gravity=np.a
     find the position of the tangent fourth sphere.
 
     :param r: Radius of the new sphere
-    :param r_1: Radius of the first sphere
-    :param x_1: `x` coordinate of the first sphere
-    :param y_1: `y` coordinate of the first sphere
-    :param z_1: `z` coordinate of the first sphere
-    :param r_2: Radius of the second sphere
-    :param x_2: `x` coordinate of the second sphere
-    :param y_2: `y` coordinate of the second sphere
-    :param z_2: `z` coordinate of the second sphere
-    :param r_3: Radius of the third sphere
-    :param x_3: `x` coordinate of the third sphere
-    :param y_3: `y` coordinate of the third sphere
-    :param z_3: `z` coordinate of the third sphere
+    :param r1: Radius of the first sphere
+    :param x1: `x` coordinate of the first sphere
+    :param y1: `y` coordinate of the first sphere
+    :param z1: `z` coordinate of the first sphere
+    :param r2: Radius of the second sphere
+    :param x2: `x` coordinate of the second sphere
+    :param y2: `y` coordinate of the second sphere
+    :param z2: `z` coordinate of the second sphere
+    :param r3: Radius of the third sphere
+    :param x3: `x` coordinate of the third sphere
+    :param y3: `y` coordinate of the third sphere
+    :param z3: `z` coordinate of the third sphere
     :param gravity: the normal vector which determines which of the two solutions is kept.
     """
-    if np.isclose(x2, x1):
-        r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3 = swap_beads(r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3)
-    plane, r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3 = swap_axes(r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3)
-    if np.isclose((x2-x1)*(y3-y1) - (x3-x1)*(y2-y1), 0):
-        raise ValueError("No solution can be computed.")
-    x, y, z = naive_resting_bead(r, r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3, s=+1)
-    a1 = are_aligned(np.array((x, y, z)), np.array((x1, y1, z1)), gravity)
-    a2 = are_aligned(np.array((x, y, z)), np.array((x1, y1, z1)), gravity)
-    a3 = are_aligned(np.array((x, y, z)), np.array((x1, y1, z1)), gravity)
+    if np.isclose(x2, x1):  # Swap first and last bead
+        r1, x1, y1, z1, r3, x3, y3, z3 = r3, x3, y3, z3, r1, x1, y1, z1
 
-    if a1 and a2  and a3:
-        return restore_swapped_axes(plane, x, y, z)
+    plane,r1,x1,y1,z1,r2,x2,y2,z2,r3,x3,y3,z3,gravity=swap_axes(r1,x1,y1,z1,r2,x2,y2,z2,r3,x3,y3,z3,gravity)
 
-    x, y, z = naive_resting_bead(r, r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3, s=-1)
-    a1 = are_aligned(np.array((x, y, z)), np.array((x1, y1, z1)), gravity)
-    a2 = are_aligned(np.array((x, y, z)), np.array((x1, y1, z1)), gravity)
-    a3 = are_aligned(np.array((x, y, z)), np.array((x1, y1, z1)), gravity)
+    xyz = np.array(naive_resting_bead(r, r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3, s=+1))
+    if is_positive_linear_combination((x1, y1, z1)-xyz,
+                                      (x2, y2, z2)-xyz,
+                                      (x3, y3, z3)-xyz,
+                                      gravity):
+        return restore_swapped_axes(plane, *xyz)
 
-    if a1 and a2 and a3:
-        return restore_swapped_axes(plane, x, y, z)
-    
-    raise ValueError(rf"¯\_(ツ)_/¯: bead cannot be properly oriented to resist {gravity = }")
+    xyz = np.array(naive_resting_bead(r, r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3, s=-1))
+    if is_positive_linear_combination((x1, y1, z1)-xyz,
+                                      (x2, y2, z2)-xyz,
+                                      (x3, y3, z3)-xyz,
+                                      gravity):
+        return restore_swapped_axes(plane, *xyz)
+
+    raise ValueError(rf"¯\_(ツ)_/¯: bead cannot be properly oriented to resist {gravity=}")
 
 
 def main():
     import pyvista as pv
 
-    x1, y1, z1, r1 = 0, 0, 1, 1
-    x2, y2, z2, r2 = 2, 2, -2, 2
-    x3, y3, z3, r3 = 3, -3, -1, 2
-    r = 3
+    x1, y1, z1, r1 = 0, 0, 0, 2
+    x2, y2, z2, r2 = 2, 0, 0, 2
+    x3, y3, z3, r3 = 0, 2, 0, 2
+    r = 2
 
     p = pv.Plotter()
     s1 = pv.Sphere(radius=r1, center=(x1, y1, z1))
@@ -117,7 +135,8 @@ def main():
     for s in (s1, s2, s3):
         p.add_mesh(s)
 
-    x_s, y_s, z_s = resting_bead(r, r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3, (0, 0, -1))
+    x_s, y_s, z_s = resting_bead(
+        r, r1, x1, y1, z1, r2, x2, y2, z2, r3, x3, y3, z3, (0, 0, -1))
 
     p.add_mesh(pv.Sphere(radius=r, center=(x_s, y_s, z_s)), color="red")
     p.show()
